7dda6182465c1e6b00388fbc1af5f0a92157d9a5
nick <2975684744@qq.com>
2020-11-23 07:46:15 -0800

    add edit entry

--- mysite/learning_logs/views.py
+++ mysite/learning_logs/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect,Http404
 from django.urls import reverse
 from .forms import TopicForm,EntryForm
-from .models import Topic
+from .models import Topic,Entry
 # Create your views here.
 
 
@@ -53,4 +53,20 @@ def new_entry(request,topic_id):
             return HttpResponseRedirect(reverse('learning_logs:topic',args=[topic_id]))
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
+
+
+
+def edit_entry(request,entry_id):
+    entry=Entry.objects.get(id=entry_id)
+    topic=entry.topic
+
+    if request.method !='POST':
+        form =EntryForm(instance=entry)
+    else:
+        form =EntryForm(instance=entry,data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic',args=[topic.id]))
+    context ={'entry':entry,'topic':topic,'form':form}
+    return render(request,'learning_logs/edit_entry.html',context)
 
