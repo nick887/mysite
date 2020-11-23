044e78914eba77fbec374d890a8362937c1b0522
nick <2975684744@qq.com>
2020-11-23 07:11:38 -0800

    add new entry

--- mysite/learning_logs/urls.py
+++ mysite/learning_logs/urls.py
@@ -10,5 +10,6 @@ urlpatterns=[
         path('topics/',views.topics,name='topics'),
         path('topics/<int:topic_id>',views.topic,name='topic'),
         path('new_topics/',views.new_topic,name='new_topic'),
+        path('new_entry/<int:topic_id>',views.new_entry,name='new_entry'),
 ]
 
