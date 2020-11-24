925b58b53e0790151a80f63bd4d9bc45266d0b9d
nick <2975684744@qq.com>
2020-11-23 19:18:58 -0800

    add register login logout

---
+++ mysite/users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from django.contrib.auth.views import LoginView,LogoutView
+
+from . import views
+
+app_name='users'
+
+urlpatterns =[
+        path('login/',LoginView.as_view(template_name='users/login.html'),name='login'),
+        path('logout/',LogoutView.as_view(template_name='learning_logs/index.html'),name='logout'),
+        path('register/',views.register,name='register'),
+]
--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import login,authenticate
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+
+
+def register(request):
+    if request.method!='POST':
+        form = UserCreationForm()
+    else:
+        form=UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user=form.save()
+            login(request,new_user)
+            return HttpResponseRedirect(reverse('learning_logs:index'))
+
+    context={'form':form}
+    return render(request,'users/register.html',context)
